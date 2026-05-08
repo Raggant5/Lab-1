7ebac9a51cad705b4f3dd25d2eb6050a4ee287b8
Sam <sam.koehler@mail.utoronto.ca>
2026-05-08 11:03:47 -0400

    Multiples.java completed

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int mult3 = 999 / 3;
+        int mult5 = 999 / 5;
+        int mult15 = 999 / 15;
+
+        System.out.println(mult3 + mult5 - mult15);
+    }
+}
