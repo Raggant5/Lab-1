35ec734e4e32acb2d37ee3c47efbe4a556745bf8
Sam <sam.koehler@mail.utoronto.ca>
2026-05-08 13:17:51 -0400

    Test commit of file not added with intellij

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(99);
-    FizzBuzz.main(args); // Is this a good solution to the problem?
+        FizzBuzz.main(args); // Is this a good solution to the problem?
         // Also why doesn't it work the first time?
         // why don't you have to push on github? why do you only have
         // to commit? - ah the reason is you are already on the server
