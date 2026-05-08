37b0a59195a9bc6025bb81e02298b41fd301895a
Sam <sam.koehler@mail.utoronto.ca>
2026-05-07 20:53:23 -0400

    lab1  completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.doFizzBuzz(99);
+    FizzBuzz.main(args); // Is this a good solution to the problem?
+        // Also why doesn't it work the first time?
     }
 }
