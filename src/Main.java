e9dd038c56666ac1218cbd83968b2f7a3bf5c4cd
Sam <sam.koehler@mail.utoronto.ca>
2026-05-08 11:07:11 -0400

    Update notes in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         FizzBuzz.doFizzBuzz(99);
     FizzBuzz.main(args); // Is this a good solution to the problem?
         // Also why doesn't it work the first time?
+        // why don't you have to push on github? why do you only have
+        // to commit? - ah the reason is you are already on the server
+        // so you are just saving locally on the server, no need to push
+        // to anything.... I wonder... could I push to my remote from git?
+        // No you cannot... you might as well (and should) just pull then.
     }
 }
